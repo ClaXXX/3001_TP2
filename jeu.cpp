7ecc189e02ffab23946bdb823670b4fae6d7266f
ClaXBot <claxbot@localhost.localdomain>
2020-12-06 23:30:07 +0100

    [Question 4] PDF + Correction apporté sur le programme pour correspondre à l'algorithme designé

--- jeu.cpp
+++ jeu.cpp
@@ -14,12 +14,11 @@ T max(T a, T b) {
 }
 
 void init(Tableau<unsigned int>& D, std::vector<unsigned int> sizes) {
-  for (unsigned int i = 0; i < sizes.at(MONSTRES); ++i)
-    D.at(i,0,0) = 0;
-  for (unsigned int i = 0; i < sizes.at(ROUGE); ++i)
-    D.at(0,i,0) = 0;
-  for (unsigned int i = 0; i < sizes.at(BLEU); ++i)
-    D.at(0,0,i) = 0;
+//  for (unsigned int i = 0; i < sizes.at(MONSTRES); ++i)
+//    D.at(i,0,0) = 0;
+  for (unsigned int j = 0; j < sizes.at(ROUGE); ++j)
+    for (unsigned int k = 0; k < sizes.at(BLEU); ++k)
+      D.at(0,j,k) = 0;
 }
 
 void fillDynamicTab(Tableau<unsigned int>& D, const std::vector<Monstre>& monstres, unsigned int magie_rouge, unsigned int magie_bleue) {
