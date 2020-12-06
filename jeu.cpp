5b43d39c9931bf0585d5894c4ee0e485950fddf9
ClaXBot <claxbot@localhost.localdomain>
2020-12-06 17:57:13 +0100

    Question4 programmationDynamique fonction

--- jeu.cpp
+++ jeu.cpp
@@ -1,34 +1,61 @@
 #include "jeu.hpp"
 
-#include <math.h>
 #include "tableau.hpp"
 
-Tableau<Monstre> Tab(const std::vector<Monstre>& monstres, unsigned int magie_rouge, unsigned int magie_bleue)
-{
-  std::vector<unsigned int> degats;
-  std::vector<unsigned int> rouge;
-  std::vector<unsigned int> bleue;
-  Tableau<unsigned int> tableau(std::vector<unsigned int>(monstres.size()));
+#define MONSTRES 0
+#define ROUGE 1
+#define BLEU 2
 
-  for (unsigned int i = 0; i < monstres.size(); i++) {
+template<typename T>
+T max(T a, T b) {
+  if (a > b)
+    return a;
+  return b;
+}
+
+void init(Tableau<unsigned int>& D, std::vector<unsigned int> sizes) {
+  for (unsigned int i = 0; i < sizes.at(MONSTRES); ++i)
+    D.at(i,0,0) = 0;
+  for (unsigned int i = 0; i < sizes.at(ROUGE); ++i)
+    D.at(0,i,0) = 0;
+  for (unsigned int i = 0; i < sizes.at(BLEU); ++i)
+    D.at(0,0,i) = 0;
+}
+
+void fillDynamicTab(Tableau<unsigned int>& D, const std::vector<Monstre>& monstres, unsigned int magie_rouge, unsigned int magie_bleue) {
+  for (unsigned int i = 0; i < monstres.size(); ++i) {
     const Monstre& monstre = monstres.at(i);
-    tableau.at(0, i, monstre.degat());
-    tableau.at()
-    degats.push_back(monstre.degat());
-    rouge.push_back(monstre.magieRouge());
-    bleue.push_back(monstre.magieBleue());
+
+    for (unsigned int j = 1; j <= magie_rouge; ++j) {
+      for (unsigned int k = 1; k <= magie_bleue; ++k) {
+        if (j >= monstre.magieRouge() && k >= monstre.magieBleue()) {
+          D.at(i+1,j,k) = max(D.at(i,j,k),
+                              monstre.degat() + D.at(i, j-monstre.magieRouge(), k-monstre.magieBleue()));
+        } else {
+          D.at(i+1,j,k) = D.at(i,j,k);
+        }
+      }
+    }
   }
 }
 
 std::vector<Monstre> programmationDynamique(const std::vector<Monstre>& monstres, unsigned int magie_rouge, unsigned int magie_bleue) {
-  // Inserez votre code ici
-  std::vector<Monstre> D{};
-  //Tableau<unsigned int> tableau(std::vector<unsigned int >(3)); // tableau à 3 dimensions
+  std::vector<unsigned int> sizes(3);
+  sizes[MONSTRES] = monstres.size() + 1;
+  sizes[ROUGE] = magie_rouge + 1;
+  sizes[BLEU] = magie_bleue + 1;
 
-  if (!magie_bleue || !magie_rouge)
-    return D;
-  for (const Monstre& monstre : monstres) {
+  Tableau<unsigned int> D(sizes);
+  std::vector<Monstre> L{};
 
+  init(D, sizes);
+  fillDynamicTab(D, monstres, magie_rouge, magie_bleue);
+  for (int i = monstres.size(); i > 0; --i) {
+    if (D.at(i,magie_rouge,magie_bleue) > D.at(i-1,magie_rouge,magie_bleue)) {
+      L.push_back(monstres.at(i-1));
+      magie_rouge-=monstres.at(i-1).magieRouge();
+      magie_bleue-=monstres.at(i-1).magieBleue();
+    }
   }
-  return D;
+  return L;
 }
